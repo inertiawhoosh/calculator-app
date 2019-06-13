1724ded0dabf8eb7eaf845037f8773dc73c1f7fd
Victoria Godfrey <victorialgodfrey@gmail.com>
2019-06-12 21:16:09 -0400

    react title

--- src/index.js
+++ src/index.js
@@ -3,4 +3,9 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
+class Doc extends React.Component{
+  componentDidMount(){
+    document.title = "calculator-app"
+  }
+
 ReactDOM.render(<App />, document.getElementById('root'));
